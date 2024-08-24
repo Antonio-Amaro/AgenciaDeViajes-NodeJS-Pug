93b775f9c7213a171380eb4477e3cabf02ffa909
Antonio Amaro <ja.antonioamaro@gmail.com>
2024-08-23 19:19:55 -0600

    actualizacion db

--- config/db.js
+++ config/db.js
@@ -3,11 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config()
 
 
-// const db = new Sequelize(process.env.DATABASE_URL, {
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.DB_HOST,
-    port: '3306',
-    dialect: 'mysql',
+const db = new Sequelize(process.env.DATABASE_URL, {
     define: {
         timestamps: false
     },
